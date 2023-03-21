53cd8ec89d42a68ba2b406754ff2c6c8deadc211
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-03-21 15:59:42 +0700

    asd

--- config/app.php
+++ config/app.php
@@ -199,7 +199,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
