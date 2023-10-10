90cec176ab185b5f6cb3d598f753741492ba46db
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-10-10 17:43:02 +0700

    encrypt

--- app/Helper/IRKHelp.php
+++ app/Helper/IRKHelp.php
@@ -80,7 +80,7 @@ class IRKHelp
         $decrypt =  Crypt::decryptString($encrypt);
         $decode = json_decode($decrypt);
 
-        return $decode;
+        return $encrypt;
     }
 
     public function ErrorResp($resultresp, $messageresp, $statuscoderesp = Response::HTTP_BAD_REQUEST){
