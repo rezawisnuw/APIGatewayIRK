a7913f2426929c69f97a373ab40c6a1822df36a2
Kalisjati <108332904+YKalisjati@users.noreply.github.com>
2023-12-14 10:04:50 +0700

    Update tests
    - Perbaikan UnitTest Presensi
    - Penambahan Mock IRK Help

--- tests/Feature/Controller/IRKPresensiGatewayTest.php
+++ tests/Feature/Controller/IRKPresensiGatewayTest.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use GuzzleHttp\Client;
 
-use App\Http\Controllers\IRKPresensiGateway;
+use App\Http\Controllers\IRK\PresensiGateway;
 use App\Http\Middleware\TokenVerify;
 
 class IRKPresensiGatewayTest extends TestCase
@@ -20,7 +20,7 @@ class IRKPresensiGatewayTest extends TestCase
     {
         $token = 'valid_token_here';
 
-        $request = Request::create('/api/live/presensi/get', 'POST');
+        $request = Request::create('/api/dev/presensi/get', 'POST');
         $request->headers->set('Authorization', 'Bearer ' . $token);
 
         $middleware = new TokenVerify();
@@ -34,32 +34,36 @@ class IRKPresensiGatewayTest extends TestCase
 
     public function testGetUnitSuccess()
     {
-        // Buat objek palsu (mock) untuk IRKPresensiGateway
-        $gatewayMock = $this->getMockBuilder(IRKPresensiGateway::class)
+        $gatewayMock = $this->getMockBuilder(IRKPresensiGatewayTest::class)
+            ->onlyMethods(['get'])
             ->disableOriginalConstructor()
             ->getMock();
 
-        // Mock hasil yang diharapkan dari metode get
         $expectedResponse = [
             'result' => 'Data has been processed',
-            'data' => [/* Data yang diharapkan di sini */],
+            'data' => [],
             'message' => 'Success on Run',
             'status' => 1,
             'statuscode' => 200
         ];
 
-        // Menggantikan metode get dengan perilaku yang diharapkan
-        $gatewayMock->method('get')->willReturn(json_encode($expectedResponse));
+        $mockRequest = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        // Buat objek Request yang dapat digunakan
+        $mockIRKHelp = new \Tests\Mocks\MockIRKHelp($mockRequest);
+        $gatewayMock->helper = $mockIRKHelp;
+
+        $gatewayMock->expects($this->once())
+            ->method('get')
+            ->willReturn(json_encode($expectedResponse));
+
         $request = $this->getMockBuilder(Request::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        // Panggil metode get
         $response = $gatewayMock->get($request);
 
-        // Verifikasi hasil yang diharapkan
         $this->assertEquals(json_encode($expectedResponse), $response);
         $this->assertJson($response);
     }
@@ -67,7 +71,8 @@ class IRKPresensiGatewayTest extends TestCase
     public function testGetUnitFailure()
     {
         // Buat objek palsu (mock) untuk IRKPresensiGateway
-        $gatewayMock = $this->getMockBuilder(IRKPresensiGateway::class)
+        $gatewayMock = $this->getMockBuilder(IRKPresensiGatewayTest::class)
+            ->onlyMethods(['get'])
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -80,7 +85,9 @@ class IRKPresensiGatewayTest extends TestCase
         ];
 
         // Menggantikan metode get dengan perilaku yang diharapkan
-        $gatewayMock->method('get')->willReturn(json_encode($expectedResponse));
+        $gatewayMock->expects($this->once())
+            ->method('get')
+            ->willReturn(json_encode($expectedResponse));
 
         // Buat objek Request yang dapat digunakan
         $request = $this->getMockBuilder(Request::class)
@@ -95,87 +102,72 @@ class IRKPresensiGatewayTest extends TestCase
         $this->assertJson($response);
     }
 
-//     public function testGetEndToEndSuccess()
-// {
-//     // Buat objek Client Guzzle
-//     $client = new \GuzzleHttp\Client();
+    public function testPostUnitSuccess()
+    {
+        // Buat objek palsu (mock) untuk IRKPresensiGateway
+        $gatewayMock = $this->getMockBuilder(IRKPresensiGatewayTest::class)
+            ->onlyMethods(['post'])
+            ->disableOriginalConstructor()
+            ->getMock();
 
-//     // Buat permintaan GET ke endpoint yang sesuai
-//     $response = $client->get('http://localhost:8000/api/dev/presensi/get', [
-//         'query' => [
-//             'userid' => 1,
-//             'code' => '2'
-//         ],
-//     ]);
+        // Mock hasil yang diharapkan dari metode post
+        $expectedResponse = [
+            'result' => 'Data has been processed',
+            'data' => [/* Data yang diharapkan di sini */],
+            'message' => 'Success on Run',
+            'status' => 1,
+            'statuscode' => 200
+        ];
 
-//     // Pastikan status respons adalah 200
-//     $this->assertEquals(200, $response->getStatusCode());
+        // Menggantikan metode post dengan perilaku yang diharapkan
+        $gatewayMock->expects($this->once())
+            ->method('post')
+            ->willReturn(json_encode($expectedResponse));
 
-//     // Dekode respons JSON
-//     $data = json_decode($response->getBody(), true);
+        // Buat objek Request yang dapat digunakan
+        $request = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-//     // Pastikan struktur respons sesuai dengan yang diharapkan
-//     $this->assertArrayHasKey('result', $data);
-//     $this->assertArrayHasKey('data', $data);
-//     $this->assertArrayHasKey('message', $data);
-//     $this->assertArrayHasKey('status', $data);
-// }
+        // Panggil metode post
+        $response = $gatewayMock->post($request);
 
-// public function testGetEndToEndFailure()
-// {
-//     // Buat objek Client Guzzle
-//     $client = new \GuzzleHttp\Client();
+        // Verifikasi hasil yang diharapkan
+        $this->assertEquals(json_encode($expectedResponse), $response);
+        $this->assertJson($response);
+    }
 
-//     // Buat permintaan GET ke endpoint yang sesuai
-//     $response = $client->get('http://localhost:8000/api/dev/presensi/get', [
-//         'query' => [
-//             'userid' => 1,
-//             'code' => '0'
-//         ],
-//     ]);
+    public function testPostUnitFailure()
+    {
+        // Buat objek palsu (mock) untuk IRKPresensiGateway
+        $gatewayMock = $this->getMockBuilder(IRKPresensiGatewayTest::class)
+            ->onlyMethods(['post'])
+            ->disableOriginalConstructor()
+            ->getMock();
 
-//     // Pastikan status respons adalah 200
-//     $this->assertEquals(200, $response->getStatusCode());
+        // Mock hasil yang diharapkan dari metode post
+        $expectedResponse = [
+            'result' => 'Mismatch',
+            'data' => [],
+            'message' => 'Failed on Run',
+            'status' => 0,
+        ];
 
-//     // Dekode respons JSON
-//     $data = json_decode($response->getBody(), true);
+        // Menggantikan metode post dengan perilaku yang diharapkan
+        $gatewayMock->expects($this->once())
+            ->method('post')
+            ->willReturn(json_encode($expectedResponse));
 
-//     // Pastikan struktur respons sesuai dengan yang diharapkan
-//     $this->assertArrayHasKey('result', $data);
-//     $this->assertArrayHasKey('data', $data);
-//     $this->assertArrayHasKey('message', $data);
-//     $this->assertArrayHasKey('status', $data);
+        // Buat objek Request yang dapat digunakan
+        $request = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-//     // Pastikan status memiliki nilai 0
-//     $this->assertEquals(0, $data['status']);
-// }
+        // Panggil metode post
+        $response = $gatewayMock->post($request);
 
-
-//     public function testGetIntegration()
-//     {
-//         $client = new \GuzzleHttp\Client();
-
-//         $response = $client->post('http://localhost:8000/api/dev/presensi/get', [
-//             'json' => [
-//                 'userid' => '2005004059',
-//                 'list_query' => [
-//                     [
-//                         'conn' => 'DBPRESENSI',
-//                         'query' => "SELECT TOP 10 * FROM PresensiIntegrationOs2 WITH(NOLOCK) WHERE personnelnumber = '2005004059';",
-//                         'process_name' => 'GetDataPresensi'
-//                     ]
-//                 ]
-//             ],
-//         ]);
-
-//         $this->assertEquals(200, $response->getStatusCode());
-
-//         $data = json_decode($response->getBody(), true);
-
-//         $this->assertArrayHasKey('result', $data);
-//         $this->assertArrayHasKey('data', $data);
-//         $this->assertArrayHasKey('message', $data);
-//         $this->assertArrayHasKey('status', $data);
-//         $this->assertArrayHasKey('statuscode', $data);
-//     }
+        // Verifikasi hasil yang diharapkan
+        $this->assertEquals(json_encode($expectedResponse), $response);
+        $this->assertJson($response);
+    }
 }
